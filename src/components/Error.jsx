6a88773355ba6a91a6f5cbbb15e03c7d9d947256
Prince Singh <princesingh808087@gmail.com>
2022-12-03 20:56:44 +0530

    removed unncessary imports and changed the backround of body

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Error = ({ error }) => {
 	return (
-		<div className="text-center mt-20">
+		<div className="text-center  text-slate-300 mt-20">
 			<h1 className="text-xl">{error}</h1>
 		</div>
 	);
