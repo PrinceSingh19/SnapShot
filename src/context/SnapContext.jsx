56c8958d558f0b53d0d56df01faaa74d3d357354
Prince Singh <princesingh808087@gmail.com>
2022-12-03 19:52:07 +0530

    created context api and added state methods in snapstate to update the states in context api

---
+++ src/context/SnapContext.jsx
@@ -0,0 +1,4 @@
+import { Context, createContext } from "react";
+
+const SnapContext = createContext();
+export default SnapContext;
---
+++ src/context/SnapState.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import SnapContext from "./SnapContext";
+
+import React from "react";
+
+const SnapState = (props) => {
+	const [images, setImages] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	const fetchImages = (query) => {
+		const url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${
+			import.meta.env.VITE_ACCESS_KEY
+		}`;
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`This is an HTTP error: The status is ${response.status}`);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data.results);
+				setImages(data.results);
+				setLoading(false);
+			})
+			.catch((error) => {
+				setLoading(false);
+				setError(error.message);
+				console.log(message);
+			});
+	};
+
+	return (
+		<SnapContext.Provider value={{ images, loading, error, fetchImages }}>
+			{props.children}
+		</SnapContext.Provider>
+	);
+};
+
+export default SnapState;
